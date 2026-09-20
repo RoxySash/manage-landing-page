d2461b5dffd02be674d892d34157991625cb40f9
Roxane Hedge <roxanehedge@gmail.com>
2026-09-20 16:39:35 -0500

    removed bg from dots

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 const hamburgerBtn = document.getElementById("hamburgerBtn");
 const closeBtn = document.getElementById("closeBtn");
 const mobileMenu = document.getElementById("mobileMenu");
@@ -14,3 +13,34 @@ closeBtn.addEventListener("click", () => {
   closeBtn.style.display = "none";
   hamburgerBtn.style.display = "flex";
 });
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  const slides = document.getElementsByClassName("mySlides");
+  const dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
